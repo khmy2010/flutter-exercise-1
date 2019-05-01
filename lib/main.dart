19b7afdd2fa2303bd1f3c159703a11af9f3be415
khmy2010 <kehan.see@gmail.com>
2019-05-01 14:06:02 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Easy PeaXsy'),
+        ),
+        body: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
