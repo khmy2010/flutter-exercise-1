4932b864f5b92ab6915c203842e8bcef8d91929f
khmy2010 <kehan.see@gmail.com>
2019-05-11 18:52:41 +0800

    progress commit

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,15 +1,61 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
+  _showWarningDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Text('Are you okay?'),
+              content: Text('This action cannot be undone.'),
+              actions: <Widget>[
+                FlatButton(
+                    child: Text('Discard'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+                FlatButton(
+                    child: Text('Continue'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context, true);
+                    })
+              ]);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Detail'),
-      ),
-      body: Center(
-        child: Text('On the product page'),
-      ),
-    );
+    return WillPopScope(
+        onWillPop: () {
+          print('Back button pressed!');
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
+              Container(padding: EdgeInsets.all(10.0)),
+              RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('Delete'),
+                onPressed: () => _showWarningDialog(context),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,45 +3,23 @@ import 'package:flutter/material.dart';
 import './products.dart';
 import './product-control.dart';
 
-class ProductManager extends StatefulWidget {
-  final String startingProduct;
+class ProductManager extends StatelessWidget {
+  final List<Map<String, String>> products;
+  final Function addProduct;
+  final Function deleteProduct;
 
-  ProductManager({this.startingProduct});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _ProductManagerState();
-  }
-}
-
-class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = [];
-
-  @override
-  void initState() {
-    if (widget.startingProduct != null) {
-      _products.add(widget.startingProduct);    
-    }
-    
-    super.initState();
-  }
+  ProductManager(this.products, this.addProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10.0),
-        child: ProductControl(_addProduct),
+        child: ProductControl(addProduct),
       ),
       Expanded(
-        child: Products(_products),
+        child: Products(products, deleteProduct: deleteProduct),
       )
     ]);
-  }
-
-  void _addProduct(String product) {
-    setState(() {
-      _products.add(product);
-    });
   }
 }
